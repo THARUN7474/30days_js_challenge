c5aa299c3dbd6c2893dda2a53d491f03624b7663
THARUN7474 <bandatharun74@gmail.com>
2024-07-14 18:30:04 +0530

    added day2

---
+++ DAY2/assign.js
@@ -0,0 +1,105 @@
+// 1. Arithmetic Operations
+
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+console.log("Sum:", sum);
+
+let difference = num1 - num2;
+console.log("Difference:", difference);
+
+
+let product = num1 * num2;
+console.log("Product:", product);
+
+
+let quotient = num1 / num2;
+console.log("Quotient:", quotient);
+
+
+let remainder = num1 % num2;
+console.log("Remainder:", remainder);
+
+console.log(
+  "**********************************************************************************"
+);
+
+// 2. Assignment Operators
+
+
+let x = 10;
+x += 5;
+console.log("After addition assignment:", x);
+
+
+let y = 20;
+y -= 8;
+console.log("After subtraction assignment:", y);
+
+console.log(
+  "**********************************************************************************"
+);
+
+// 3. Comparison Operators
+
+let a = 25;
+let b = 15;
+console.log("a > b:", a > b); 
+console.log("a < b:", a < b); 
+
+console.log("a >= b:", a >= b); 
+console.log("a <= b:", a <= b); 
+
+console.log(
+  "**********************************************************************************"
+);
+
+// 4. Logical Operators
+
+// Task 10: && operator (AND)
+let condition1 = true;
+let condition2 = false;
+console.log("condition1 && condition2:", condition1 && condition2);
+
+// Task 11: || operator (OR)
+console.log("condition1 || condition2:", condition1 || condition2);
+
+// Task 12: ! operator (NOT)
+console.log("!condition1:", !condition1);
+
+console.log(
+  "**********************************************************************************"
+);
+
+// 5. Ternary Operator
+
+let number = -8;
+let result1 = number >= 0 ? "Positive" : "Negative";
+console.log("Number is:", result1);
+
+let number2 = 0;
+let result2 = number2 >= 0 ? "Positive" : "Negative";
+console.log("Number is:", result2);
+
+
+
+
+
+// / Comparison and Logical Operators Script
+
+// // Define two numbers
+// let a = 25;
+// let b = 10;
+
+// // Comparison operators
+// console.log(`${a} > ${b} is:`, a > b);
+// console.log(`${a} < ${b} is:`, a < b);
+// console.log(`${a} >= ${b} is:`, a >= b);
+// console.log(`${a} <= ${b} is:`, a <= b);
+
+// // Logical operators
+// let condition11 = true;
+// let condition22 = false;
+// console.log(`Condition 1 && Condition 2 is:`, condition11 && condition22);
+// console.log(`Condition 1 || Condition 2 is:`, condition11 || condition22);
+// console.log(`!Condition 1 is:`, !condition11);
